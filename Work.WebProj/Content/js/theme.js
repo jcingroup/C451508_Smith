aef7b5ef57f9d08c6862c0d9febcba14d9d5a228
TinWu <yyuuix@gmail.com>
2015-11-10 16:03:26 +0800

    行動裝置選單

--- Work.WebProj/Content/js/theme.js
+++ Work.WebProj/Content/js/theme.js
@@ -1,10 +1,10 @@
 // 沒有卷軸時不出現 goTop 按鈕
 $(window).scroll(function(){
-     if ($(this).scrollTop() > 100) {
-          $('.goTop').fadeIn();
-     } else {
-          $('.goTop').fadeOut();
-     }
+    if ($(this).scrollTop() > 100) {
+        $('.goTop').fadeIn();
+    } else {
+        $('.goTop').fadeOut(500);
+    }
 });
 
 // 點選後跳到 href 指向的位置
@@ -15,14 +15,22 @@ $('.goTop').click(function () {
     return false;
 });
 
-// 行動裝置的主選單
 $(document).ready(function() {
+    // 行動裝置的主選單
     $menuLeft = $('#menu');
     $trigger = $('.menu-trigger');
 
     $trigger.click(function() {
         $(this).toggleClass('active');
-        $('body').toggleClass('pushmenu-push');
-        $menuLeft.toggleClass('pushmenu-open');
+        $('body').toggleClass('push');
+    });
+    $('.toggle').click(function() {
+        $('body').removeClass('push');
+    });
+
+    // 行動裝置的產品分類選單
+    $("aside header").click(function() {
+        $(this).toggleClass("active");
+        $(this).next().slideToggle();
     });
 });
