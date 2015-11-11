57ffd89b44e3bd2a69c00b5df14bdec3346f7b4c
Ajoe <sdec0817@gmail.com>
2015-11-11 15:16:06 +0800

    前台 產品列套程式

--- Work.WebProj/Controllers/IndexController.cs
+++ Work.WebProj/Controllers/IndexController.cs
@@ -10,14 +10,26 @@ using System.Net.Mail;
 using ProcCore.HandleResult;
 using DotWeb.CommSetup;
 using DotWeb.Controller;
+using ProcCore.Business.DB0;
+
 namespace DotWeb.Controllers
 {
     public class IndexController : WebUserController
     {
         public ActionResult Index()
         {
+            List<option> items = new List<option>();
+            using (var db0 = getDB0())
+            {
 
-            return View();
+                items = db0.ProductCategory.Where(x => !x.i_Hide).OrderByDescending(x => x.sort)
+                                                             .Select(x => new option()
+                                                             {
+                                                                 val = x.product_category_id,
+                                                                 Lname = x.category_name
+                                                             }).Take(6).ToList();
+            }
+            return View(items);
         }
 
         public RedirectResult Login()
